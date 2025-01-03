b404661c3b132d53f1a73eade05b464dc9fef4d2
Zeus <divyamsheth4@gmail.com>
2025-01-03 23:09:41 +0100

    textureless mesh  generates

--- volume.py
+++ volume.py
@@ -1,31 +1,74 @@
 import torch
 import numpy as np
 import cv2
+import glob
+import open3d as o3d
+from skimage import measure
 
+
+def rigid_transform(xyz, transform):
+  """Applies a rigid transform to an (N, 3) pointcloud.
+  """
+  xyz_h = np.hstack([xyz, np.ones((len(xyz), 1), dtype=np.float32)])
+  xyz_t_h = np.dot(transform, xyz_h.T).T
+  return xyz_t_h[:, :3]
+
+def get_view_frustum(depth_im, cam_intr, cam_pose):
+  """Get corners of 3D camera view frustum of depth image
+  """
+  im_h = depth_im.shape[0]
+  im_w = depth_im.shape[1]
+  max_depth = np.max(depth_im)
+  view_frust_pts = np.array([
+    (np.array([0,0,0,im_w,im_w])-cam_intr[0,2])*np.array([0,max_depth,max_depth,max_depth,max_depth])/cam_intr[0,0],
+    (np.array([0,0,im_h,0,im_h])-cam_intr[1,2])*np.array([0,max_depth,max_depth,max_depth,max_depth])/cam_intr[1,1],
+    np.array([0,max_depth,max_depth,max_depth,max_depth])
+  ])
+  view_frust_pts = rigid_transform(view_frust_pts.T, cam_pose).T
+  return view_frust_pts
 
 class TSDF():
 
-    def __init__(self, vol_dim=[100, 100, 100], voxel_size=2):
+    def __init__(self, vol_dim, voxel_size=0.01):
 
-        self.vol_dim = vol_dim
-        x_grid = torch.arange(0, self.vol_dim[0])
-        y_grid = torch.arange(0, self.vol_dim[1])
-        z_grid = torch.arange(0, self.vol_dim[2])
+        # self.vol_dim = vol_dim
+        self._vol_bnds = vol_dim
+        self._voxel_size = voxel_size
+        print(self._vol_bnds )
+        self._vol_dim = np.ceil((self._vol_bnds[:,1]-self._vol_bnds[:,0])/self._voxel_size).copy(order='C').astype(int)
+        self._vol_bnds[:,1] = self._vol_bnds[:,0]+self._vol_dim*self._voxel_size
+        self._vol_origin = self._vol_bnds[:,0].copy(order='C').astype(np.float32)
+        print( self._vol_origin)
+        print("Voxel volume size: {} x {} x {} - # points: {:,}".format(
+        self._vol_dim[0], self._vol_dim[1], self._vol_dim[2],
+        self._vol_dim[0]*self._vol_dim[1]*self._vol_dim[2])
+        )
+            
+        x_grid = torch.arange(0, self._vol_dim[0])
+        y_grid = torch.arange(0, self._vol_dim[1])
+        z_grid = torch.arange(0, self._vol_dim[2])
         xv, yv, zv = torch.meshgrid([x_grid, y_grid, z_grid])
         self.vox_coords = torch.stack(
             [xv.flatten(), yv.flatten(), zv.flatten()], dim=1).double()
+
+        self.vox_Wcoords = torch.cat([
+            (self.vox_coords * voxel_size) + self._vol_origin, torch.ones(len(self.vox_coords), 1, )], dim=1).double()
+
         self.vox_coords = torch.cat([
             self.vox_coords, torch.ones(len(self.vox_coords), 1, )], dim=1).double()
+
         self.sdf_values = torch.ones(
-            (self.vol_dim[0], self.vol_dim[1], self.vol_dim[2])).double()
-        self.weights = torch.ones(
-            (self.vol_dim[0], self.vol_dim[1], self.vol_dim[2])).double()
-        print(self.vox_coords.shape)
+            (self._vol_dim[0], self._vol_dim[1], self._vol_dim[2])).double()
 
-    def integrate(self, depth_image, camera_pose, intristics, trunc_value, sdf_trunc):
+        self.weights = torch.zeros(
+            (self._vol_dim[0], self._vol_dim[1], self._vol_dim[2])).double()
 
-        pts_camera = torch.matmul(torch.from_numpy(
-            camera_pose), torch.t(self.vox_coords))
+    def integrate(self, depth_image, camera_pose, intristics, sdf_trunc=0.03):
+
+        world2cam = torch.inverse(torch.from_numpy(camera_pose))
+        # print(torch.inverse(torch.from_numpy(camera_pose)))
+        pts_camera = torch.matmul(
+            world2cam, torch.t(self.vox_Wcoords))
         z_points = pts_camera[2]
         fx = intristics[0][0]
         fy = intristics[1][1]
@@ -39,49 +82,100 @@ class TSDF():
         x_coords_valid = self.vox_coords[valid_pix, 0]
         y_coords_valid = self.vox_coords[valid_pix, 1]
         z_coords_valid = self.vox_coords[valid_pix, 2]
+        
+        z_pix = pts_camera[2, valid_pix]
         depth_val = depth_image[y_pix[valid_pix], x_pix[valid_pix]]
 
-        depth_diff = depth_val - z_coords_valid
-
-        valid_sdf = (depth_diff <= trunc_value) & (depth_diff >= -trunc_value)
+        depth_diff = depth_val - z_pix
 
         dist = torch.clamp(depth_diff / sdf_trunc, max=1)
 
-        valid_pix = (depth_val > 0) & (depth_diff > -sdf_trunc)
-        x_points_valid = x_coords_valid[valid_sdf].int()
-        y_points_valid = y_coords_valid[valid_sdf].int()
-        z_points_valid = z_coords_valid[valid_sdf].int()
-        valid_dist = dist[valid_sdf]
+        valid_pts = (depth_val > 0.) & (depth_diff >= -sdf_trunc)
+
+        x_points_valid = x_coords_valid[valid_pts].int()
+        y_points_valid = y_coords_valid[valid_pts].int()
+        z_points_valid = z_coords_valid[valid_pts].int()
+        valid_dist = dist[valid_pts]
+
         old_sdf = self.sdf_values[x_points_valid, y_points_valid,
-                                  y_points_valid]
+                                  z_points_valid]
         old_weights = self.weights[x_points_valid, y_points_valid,
-                                        y_points_valid]
-        
+                                   z_points_valid]
+
         self.sdf_values[x_points_valid, y_points_valid,
-                                  y_points_valid] = ((old_weights * old_sdf) + valid_dist)/(old_weights + 1 )
-        
+                        z_points_valid] = ((old_weights * old_sdf) + valid_dist)/(old_weights + 1)
+
         self.weights[x_points_valid, y_points_valid,
-                                        y_points_valid] = self.weights[x_points_valid, y_points_valid,
-                                        y_points_valid] +1 
+                     z_points_valid] = self.weights[x_points_valid, y_points_valid, z_points_valid] + 1
         
-        # depth_values
-        # print(pts_camera[2])
-        # get gridspace into pixel space.
+        print("SDF min:", self.sdf_values.min())
+        print("SDF max:", self.sdf_values.max())
+
         return 0
 
 
 if __name__ == "__main__":
 
-    test = np.random.random(10)
-    new = [True, True, True, True, False, True, True, False, False, False]
-    print(test[new])
-    depth_im = cv2.imread("data/frame-%06d.depth.png" % (0), -1).astype(float)
-    depth_im = torch.from_numpy(depth_im)
-    depth_im /= 1000.
-    depth_im[depth_im == 65.535] = 0
-    print(depth_im.shape)
     cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
-    cam_pose = np.loadtxt("data/frame-%06d.pose.txt" % (0))
-    vox_grid = TSDF()
-    vox_grid.integrate(depth_im, cam_pose, cam_intr,
-                       trunc_value=1, sdf_trunc=1)
+    worldpose= np.identity(4)
+
+    print("Estimating voxel volume bounds...")
+    n_imgs = 10
+    cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
+    vol_bnds = np.zeros((3,2))
+    for i in range(n_imgs):
+        # Read depth image and camera pose
+
+        depth_im = cv2.imread("data/frame-%06d.depth.png"%(i),-1).astype(float)
+        depth_im /= 1000.  # depth is saved in 16-bit PNG in millimeters
+        depth_im[depth_im == 65.535] = 0  # set invalid depth to 0 (specific to 7-scenes dataset)
+        cam_pose = np.loadtxt("data/frame-%06d.pose.txt"%(i))  # 4x4 rigid transformation matrix
+        if i == 0:
+            worldpose = cam_pose
+
+        # Compute camera view frustum and extend convex hull
+        view_frust_pts = get_view_frustum(depth_im, cam_intr, cam_pose)
+        vol_bnds[:,0] = np.minimum(vol_bnds[:,0], np.amin(view_frust_pts, axis=1))
+        vol_bnds[:,1] = np.maximum(vol_bnds[:,1], np.amax(view_frust_pts, axis=1))
+
+    print("Initializing voxel volume...")
+    vox_grid = TSDF(vol_bnds, voxel_size=0.02)
+
+    path_to_folder = "/home/zeus/masters/3DSMC/project/TSDF/data"
+
+    depths = sorted(glob.glob(path_to_folder + "/" + "*.depth.png"))
+    poses = sorted(glob.glob(path_to_folder + "/" + "*.pose.txt"))
+
+    for depth, pose in zip(depths, poses):
+        depth_im = cv2.imread(depth, -1).astype(float)
+        depth_im = torch.from_numpy(depth_im)
+
+        depth_im /= 1000.
+        depth_im[depth_im == 65.535] = 0
+        cam_pose = np.loadtxt(pose)
+        vox_grid.integrate(depth_im, cam_pose, cam_intr)
+
+    sdf_numpy = vox_grid.sdf_values.numpy()
+
+    print("SDF min:", sdf_numpy.min())
+    print("SDF max:", sdf_numpy.max())
+
+    # vertices, faces, normals, _ = measure.marching_cubes(
+    #     sdf_numpy, level=0.0)
+
+    # Scale vertices to match your voxel grid's physical size
+    voxel_size = 0.02
+    # vertices *= voxel_size
+
+    verts, faces, norms, vals = measure.marching_cubes(sdf_numpy, level=0)
+    verts_ind = np.round(verts).astype(int)
+    verts = verts*voxel_size 
+
+
+
+    mesh = o3d.geometry.TriangleMesh()
+    mesh.vertices = o3d.utility.Vector3dVector(verts)
+    mesh.triangles = o3d.utility.Vector3iVector(faces)
+    mesh.compute_vertex_normals()
+
+    o3d.visualization.draw_geometries([mesh])
