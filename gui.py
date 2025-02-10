31e86d0ff17181e2aef1b69b86357d2d94b6c341
Svenja <thimmsvenja@web.de>
2025-02-10 21:33:36 +0100

    integrated gui...again

--- gui.py
+++ gui.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 from skimage import measure
 from scipy.interpolate import RegularGridInterpolator
@@ -23,6 +22,7 @@ class MeshWidget(QOpenGLWidget):
         self.rotX = 0.0
         self.rotY = 0.0
         self.rotZ = 0.0
+        self.scale = 0.0
 
         self.verts = []
         self.faces = []
@@ -61,6 +61,9 @@ class MeshWidget(QOpenGLWidget):
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
 
         gl.glEnable(gl.GL_DEPTH_TEST)
+        gl.glEnable(gl.GL_COLOR_MATERIAL)
+        gl.glEnable(gl.GL_NORMALIZE)
+        gl.glColorMaterial(gl.GL_FRONT_AND_BACK, gl.GL_AMBIENT_AND_DIFFUSE)
         gl.glEnable(gl.GL_LIGHTING)
         gl.glEnable(gl.GL_LIGHT0)
         gl.glLightfv(gl.GL_LIGHT0, gl.GL_POSITION, [2., 2., -10., 0.])
@@ -72,7 +75,7 @@ class MeshWidget(QOpenGLWidget):
         gl.glPushMatrix()
 
         gl.glTranslate(0.0, 0.0, -5.0)
-        gl.glScale(3.0, 3.0, 3.0)
+        gl.glScale(3.0+self.scale, 3.0+self.scale, 3.0+self.scale)
         gl.glRotate(self.rotX+180, 1.0, 0.0, 0.0)
         gl.glRotate(self.rotY, 0.0, 1.0, 0.0)
         gl.glRotate(self.rotZ, 0.0, 0.0, 1.0)
@@ -96,25 +99,27 @@ class MeshWidget(QOpenGLWidget):
         gl.glPopMatrix()
 
     def write_mesh_file(self):
-        filename = "reconstruction.off"
-        with open(filename, 'w') as f:
-            if self.color_mode:
-                f.write("COFF\n")
-            else:
-                f.write("OFF\n")
-            f.write(f"{len(self.verts)} {len(self.faces)} 0\n")
-
-            for i, v in enumerate(self.verts):
-                vertex_str = f"{v[0]} {v[1]} {v[2]}"
+        if self.vox_grid is not None:
+            filename = "reconstruction.off"
+            with open(filename, 'w') as f:
                 if self.color_mode:
-                    color_int = (self.vertex_colors[i] * 255).astype(int)
-                    color_str = ' '.join(map(str, color_int))
-                    f.write(f"{vertex_str} {color_str}\n")
+                    f.write("COFF\n")
                 else:
-                    f.write(f"{vertex_str}\n")
+                    f.write("OFF\n")
+                f.write(f"{len(self.verts)} {len(self.faces)} 0\n")
 
-            for face in self.faces:
-                f.write(f"{len(face)} {' '.join(map(str, face))}\n")
+                for i, v in enumerate(self.verts):
+                    vertex_str = f"{v[0]} {v[1]} {v[2]}"
+                    if self.color_mode:
+                        color_int = (self.vertex_colors[i] * 255).astype(int)
+                        color_str = ' '.join(map(str, color_int))
+                        f.write(f"{vertex_str} {color_str}\n")
+                    else:
+                        f.write(f"{vertex_str}\n")
+
+                for face in self.faces:
+                    f.write(f"{len(face)} {' '.join(map(str, face))}\n")
+            print("saved mesh to ", filename)
 
     def get_mesh(self):
         if self.vox_grid is not None:
@@ -124,28 +129,29 @@ class MeshWidget(QOpenGLWidget):
             # voxel_size = 20
 
             verts, faces, norms, vals = measure.marching_cubes(sdf_numpy, level=0)
-            verts_ind = np.round(verts).astype(int)
             self.verts = verts * voxel_size
             self.faces = faces
 
-            # TODO this crashes regularly
-            #y = np.arange(color_sdf.shape[1]) * voxel_size
-            #x = np.arange(color_sdf.shape[0]) * voxel_size
-            #z = np.arange(color_sdf.shape[2]) * voxel_size
+            if self.color_mode:
+                x = np.arange(color_sdf.shape[0]) * voxel_size
+                y = np.arange(color_sdf.shape[1]) * voxel_size
+                z = np.arange(color_sdf.shape[2]) * voxel_size
 
-            #r_interpolator = RegularGridInterpolator((x, y, z), color_sdf[..., 0])
-            #g_interpolator = RegularGridInterpolator((x, y, z), color_sdf[..., 1])
-            #b_interpolator = RegularGridInterpolator((x, y, z), color_sdf[..., 2])
+                r_interpolator = RegularGridInterpolator((x, y, z), color_sdf[..., 0], bounds_error=False, fill_value=0)
+                g_interpolator = RegularGridInterpolator((x, y, z), color_sdf[..., 1], bounds_error=False, fill_value=0)
+                b_interpolator = RegularGridInterpolator((x, y, z), color_sdf[..., 2], bounds_error=False, fill_value=0)
 
-            #r_values = r_interpolator(verts)
-            #g_values = g_interpolator(verts)
-            #b_values = b_interpolator(verts)
+                r_values = r_interpolator(self.verts)
+                g_values = g_interpolator(self.verts)
+                b_values = b_interpolator(self.verts)
 
-            #self.vertex_colors = np.stack((b_values, g_values, r_values), axis=-1)
-            default_color = np.array([0.5, 0.5, 1.0])  # RGB color for blue
+                self.vertex_colors = np.stack((r_values, g_values, b_values), axis=-1) / 255.
 
-            # Set all vertex colors to the default color
-            self.vertex_colors = np.tile(default_color, (self.verts.shape[0], 1))
+            else:
+                default_color = np.array([0.5, 0.5, 1.0])  # RGB color for blue
+
+                # Set all vertex colors to the default color
+                self.vertex_colors = np.tile(default_color, (self.verts.shape[0], 1))
 
             min_bound = np.min(self.verts, axis=0)
             max_bound = np.max(self.verts, axis=0)
@@ -179,6 +185,15 @@ class MeshWidget(QOpenGLWidget):
         self.rotZ = value
         self.update()
 
+    def set_scale(self, value):
+        self.scale = value
+        self.update()
+
+    def set_color_mode(self, value):
+        self.color_mode = value
+        self.get_mesh()
+        self.update()
+
     def reset(self):
         self.vox_grid = None
 
@@ -202,17 +217,21 @@ class MainWindow(QMainWindow):
     def setup_gui(self):
         self.setWindowTitle("Kinect Fusion")
         central_widget = QWidget()
-        xSlider = QSlider(Qt.Horizontal)
-        ySlider = QSlider(Qt.Horizontal)
-        zSlider = QSlider(Qt.Horizontal)
+        xSlider = QSlider(Qt.Orientation.Horizontal)
+        ySlider = QSlider(Qt.Orientation.Horizontal)
+        zSlider = QSlider(Qt.Orientation.Horizontal)
+        sSlider = QSlider(Qt.Orientation.Horizontal)
         xSlider.setRange(-360, 360)
         xSlider.setValue(0)
         ySlider.setRange(-360, 360)
         ySlider.setValue(0)
         zSlider.setRange(-360, 360)
         zSlider.setValue(0)
+        sSlider.setRange(-2, 20)
+        sSlider.setValue(0)
+        self.sliders = [xSlider, ySlider, zSlider, sSlider]
 
-        self.sliders = [xSlider, ySlider, zSlider]
+        colorChx = QCheckBox("Color mode")
 
         reset_btn = QPushButton("Reset")
         save_btn = QPushButton("Save Mesh")
@@ -221,17 +240,23 @@ class MainWindow(QMainWindow):
         buttonLayout.addWidget(xSlider)
         buttonLayout.addWidget(ySlider)
         buttonLayout.addWidget(zSlider)
+        buttonLayout.addWidget(sSlider)
+        buttonLayout.addWidget(colorChx)
         buttonLayout.addWidget(reset_btn)
         buttonLayout.addWidget(save_btn)
 
         input_layout = QVBoxLayout()
         input_layout.addWidget(self.color_panel)
         input_layout.addWidget(self.depth_panel)
-        input_layout.addLayout(buttonLayout)
+        #input_layout.addLayout(buttonLayout)
 
-        main_layout = QHBoxLayout()
-        main_layout.addWidget(self.mesh_panel)
-        main_layout.addLayout(input_layout)
+        panelLayout = QHBoxLayout()
+        panelLayout.addWidget(self.mesh_panel)
+        panelLayout.addLayout(input_layout)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(panelLayout)
+        main_layout.addLayout(buttonLayout)
 
         central_widget.setLayout(main_layout)
         self.setCentralWidget(central_widget)
@@ -241,6 +266,8 @@ class MainWindow(QMainWindow):
         xSlider.valueChanged.connect(self.mesh_panel.set_rot_x)
         ySlider.valueChanged.connect(self.mesh_panel.set_rot_y)
         zSlider.valueChanged.connect(self.mesh_panel.set_rot_z)
+        sSlider.valueChanged.connect(self.mesh_panel.set_scale)
+        colorChx.toggled.connect(self.mesh_panel.set_color_mode)
 
     def closeEvent(self, event):
         if self.sensor.is_running:
@@ -254,10 +281,12 @@ class MainWindow(QMainWindow):
     def reset(self):
         self.sensor.reset()
         self.mesh_panel.reset()
+        for s in self.sliders:
+            s.setValue(0)
 
     def get_img_from_frame(self, frame):
         color_map = frame[0].cpu().numpy()
-        depth_map = frame[1][0].cpu().numpy()
+        depth_map = frame[1].cpu().numpy()
 
         h, w, ch = color_map.shape
         bytes_per_line = ch * w
